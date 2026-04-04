4cc65c86b98d64eb3575a7cc24edb60a83951c48
Eli Song <eli.ehnj2@gmail.com>
2026-04-03 20:55:36 -0400

    changing linting config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,15 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
 ];
 
 export default eslintConfig;
